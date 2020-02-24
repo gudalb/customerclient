d75606b4e7827f062215500202d15516f6cd3e2a
patrikellboj <patrikellboj@gmail.com>
2020-02-24 18:50:07 +0100

    Added paymentplan and some dummy data

--- filltables.sql
+++ filltables.sql
@@ -1,5 +1,5 @@
 use inl3;
-
+-- customers
 insert into customer(name, password, ssnumber)
 values("albin", "123", "123");
 
@@ -9,41 +9,111 @@ values("fred", "123", "456");
 insert into customer(name, password, ssnumber)
 values("klas", "123", "789");
 
+insert into customer(name, password, ssnumber)
+values("Erik", "123", "900101");
+
+insert into customer(name, password, ssnumber)
+values("Stefan", "123", "900102");
+
+insert into customer(name, password, ssnumber)
+values("Jan", "123", "900103");
+
+-- admins
 insert into staff(name, password)
-values("greta", "123");
+values("Albin", "123");
 
 insert into staff(name, password)
-values("greta", "123");
+values("Patrik", "123");
 
 insert into staff(name, password)
-values("greta", "123");
+values("Daniel", "123");
 
-
+-- Customer accounts
 insert into account(customer_id, balance, interest_rate)
 	values(1,1000,0.5);
 
+insert into account(customer_id, balance, interest_rate)
+	values(1,2500,3.3);
 
 insert into account(customer_id, balance, interest_rate)
-	values(1,2500,0.5);
-
-
-insert into account(customer_id, balance, interest_rate)
-	values(2,1000,0.5);
-
+	values(2,10000,5);
 
 insert into account(customer_id, balance, interest_rate)
-	values(2,45000,0.5);
-
+	values(2,45000,0.2);
 
 insert into account(customer_id, balance, interest_rate)
 	values(3,500,0.5);
 
-
+-- Customer loans
 insert into loan(customer_id, amount, interest_rate, staff_approved_id)
 	values(1,25000,3,1);
 
-
 insert into loan(customer_id, amount, interest_rate, staff_approved_id)
 	values(2,37000,3,null);
+    
+insert into loan(customer_id, amount, interest_rate, staff_approved_id)
+	values(3,45000,3.35,null);
 
+insert into loan(customer_id, amount, interest_rate, staff_approved_id)
+	values(4,75000,3.35,1);
+    
+insert into loan(customer_id, amount, interest_rate, staff_approved_id)
+	values(4,12000,5,null);
+    
+insert into loan(customer_id, amount, interest_rate, staff_approved_id)
+	values(4,4500,4.5,null);
 
+insert into loan(customer_id, amount, interest_rate, staff_approved_id)
+	values(5,33000,3,2);
+    
+insert into loan(customer_id, amount, interest_rate, staff_approved_id)
+	values(5,80500,1.2,null);
+    
+insert into loan(customer_id, amount, interest_rate, staff_approved_id)
+	values(5,17500,1.1,null);
+
+insert into loan(customer_id, amount, interest_rate, staff_approved_id)
+	values(6,500,10,3);
+
+insert into loan(customer_id, amount, interest_rate, staff_approved_id)
+	values(6,14500,4,null);
+    
+insert into loan(customer_id, amount, interest_rate, staff_approved_id)
+	values(6,110000,0.4,null);
+    
+-- customer paymentplans
+insert into paymentPlan(loan_id, years)
+	values(1, 4);
+
+insert into paymentPlan(loan_id, years)
+	values(2, 5);
+
+insert into paymentPlan(loan_id, years)
+	values(3, 7);
+
+insert into paymentPlan(loan_id, years)
+	values(4, 6);
+    
+insert into paymentPlan(loan_id, years)
+	values(5, 2);
+
+insert into paymentPlan(loan_id, years)
+	values(6, 5);
+    
+insert into paymentPlan(loan_id, years)
+	values(7, 6);
+    
+insert into paymentPlan(loan_id, years)
+	values(8, 10);
+    
+insert into paymentPlan(loan_id, years)
+	values(9, 3);
+    
+insert into paymentPlan(loan_id, years)
+	values(10, 2);
+    
+insert into paymentPlan(loan_id, years)
+	values(11, 7);
+    
+insert into paymentPlan(loan_id, years)
+	values(12, 10);
--- tables.sql
+++ tables.sql
@@ -1,21 +1,22 @@
+DROP DATABASE IF EXISTS inl3;
 create database inl3;
-	use inl3;
+	USE inl3;
 
-	create table if not exists customer
+	create table customer
 		(id int not null auto_increment,
 			name varchar(30) not null,
 			ssnumber varchar(12) not null,
 			password varchar(30) not null,
 			primary key (id));
 
-	create table if not exists staff
+	create table staff
 		(id int not null auto_increment,
 			name varchar(30) not null,
 			password varchar(30) not null,
 			primary key(id));
 
 
-	create table if not exists account
+	create table account
 		(id int not null auto_increment,
 			customer_id int not null,
 			balance double default 0,
@@ -23,7 +24,7 @@ create database inl3;
 			primary key (id),
 			foreign key (customer_id) references customer(id));
 
-	create table if not exists loan
+	create table loan
 		(id int not null auto_increment,
 			customer_id int not null,
 			amount double not null,
@@ -32,3 +33,11 @@ create database inl3;
 			primary key (id),
 			foreign key (customer_id) references customer(id),
 			foreign key (staff_approved_id) references staff(id));
+            
+	create table paymentPlan (
+		id int NOT NULL auto_increment,
+		loan_id int NOT NULL,
+		years double NOT NULL,
+		PRIMARY KEY(id), 
+		FOREIGN KEY (loan_id) REFERENCES loan(id)
+);
